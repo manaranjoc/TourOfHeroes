a71d335e8507ce3adbfc7fce256545dc16d60699
Miguel Angel <manaranjoc@alcazares.edu.co>
2019-07-06 23:22:18 -0500

    Crear componente angular material y ajustar navegavilidad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HeroesComponent} from './heroes/heroes.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import {AngularmaterialComponent} from './angularmaterial/angularmaterial.component';
 
 const routes: Routes = [
   {path: 'heroes', component:HeroesComponent},
   {path: 'dashboard', component:DashboardComponent},
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'detail/:id', component: HeroDetailComponent}
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: 'angular_material', component:AngularmaterialComponent}
 ];
 
 @NgModule({
